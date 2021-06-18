c3278a9b5b88d85cc53b0e112023a1cc9f48a493
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2021-06-18 13:54:36 -0400

    Edit version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setuptools.setup(
     name="misclib",
-    version='0.0.1',
+    version='0.0.2',
     description="A library of miscellaneous functions.",
     long_description=long_description,
     long_description_content_type="text/markdown",
@@ -20,5 +20,5 @@ setuptools.setup(
     ],
     package_dir={"": "src"},
     packages=setuptools.find_packages(where="src"),
-    python_requires=">=3.9",
+    python_requires=">=3.9,<4.0",
 )
