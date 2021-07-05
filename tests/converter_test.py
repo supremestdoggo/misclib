5353c31b6d02b2eacf16629a03d6c448753911b7
supremestdoggo <83146042+supremestdoggo@users.noreply.github.com>
2021-07-05 09:57:00 -0400

    Edited file structure

--- tests/converter_test.py
+++
@@ -1,6 +0,0 @@
-from misclib.converter import Converter
-
-def test_converter():
-    converter = Converter()
-    converter.fit(["Hi!", "Hello World!", "foo", "bar"])
-    assert converter.itos(converter.stoi("foobar")) == "foobar"
